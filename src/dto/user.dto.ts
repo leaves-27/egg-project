5a6caa010b5238fcf1b7c0551c52e409b8e59f61
dongkeke <dongkeke@zgmmtuan.com>
2022-07-09 23:23:06 +0800

    异常提示修改

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,8 +1,8 @@
 import { Rule, RuleType } from '@midwayjs/validate';
 export class UserLoginDTO {
-  @Rule(RuleType.string().max(100).min(3).error(new Error('userName错误')))
+  @Rule(RuleType.string().max(100).min(3).error(new Error('用户名格式错误')))
   userName: string;
 
-  @Rule(RuleType.string().max(12).min(6).error(new Error('password错误')))
+  @Rule(RuleType.string().max(12).min(6).error(new Error('密码格式错误')))
   password: string;
 }
