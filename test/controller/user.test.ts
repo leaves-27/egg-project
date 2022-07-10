3ede954bde64e5b44c7f5ef5a391886fb5d63068
dongkeke <dongkeke@zgmmtuan.com>
2022-07-10 21:43:31 +0800

    修改测试代码

--- test/controller/user.test.ts
+++ test/controller/user.test.ts
@@ -3,104 +3,114 @@ import { Framework } from "@midwayjs/web";
 import { Application } from "egg";
 
 describe("test/controller/user.test.ts", ()=>{
-  // describe("/api/user/login", () => {
-  //   // Case1: 登录成功
-  //   // Case2: 登录失败
-  //   // Case3: 用户名或密码格式不正确
+  let app: Application;
 
-  //   // 测试用例1
-  //   it("用户名和密码输入均正确", async () => {
-  //     // case1: 登录成功
-  //     const result = await createHttpRequest(app)
-  //     .post("/api/user/login").send({ 
-  //       userName: 'jack',
-  //       password: 'testtest' 
-  //     });
-  //     expect(result.status).toBe(200);
-  //     const json = JSON.parse(result.text);
-  //     expect(json.success).toBe(true);
-  //     expect(typeof json.data).toBe('string'); //非空字符串
-  //   });
-  //   it("should POST /api/user/login 密码输入不匹配", async () => {
-  //     // case2_1: 密码不匹配
-  //     const result = await createHttpRequest(app)
-  //     .post("/api/user/login").send({ 
-  //       userName: 'jack',
-  //       password: 'test11' 
-  //     });
+  beforeAll(async () => {
+    // create app
+    app = await createApp<Framework>();
+  });
 
-  //     expect(result.status).toBe(200);
+  afterAll(async () => {
+    await close(app);
+  });
+  describe("POST /api/user/login", () => {
+    // Case1: 登录成功
+    // Case2: 登录失败
+    // Case3: 用户名或密码格式不正确
 
-  //     const json = JSON.parse(result.text);
-  //     expect(json.success).toBe(false);
-  //     expect(json.data).toBeNull();
-  //   });
+    // 测试用例1
+    it("用户名和密码输入均正确", async () => {
+      // case1: 登录成功
+      const result = await createHttpRequest(app)
+      .post("/api/user/login").send({ 
+        userName: 'jack',
+        password: 'testtest' 
+      });
+      expect(result.status).toBe(200);
+      const json = JSON.parse(result.text);
+      expect(json.success).toBe(true);
+      expect(typeof json.data).toBe('string'); //非空字符串
+    });
+    it("密码输入不匹配,即是未注册的用户", async () => {
+      // case2_1: 密码不匹配
+      const result = await createHttpRequest(app)
+      .post("/api/user/login").send({ 
+        userName: 'jack',
+        password: 'test11' 
+      });
 
-  //   it("should POST /api/user/login 用户名输入不匹配", async () => {
-  //     // case2_2: 用户名不匹配
-  //     const result = await createHttpRequest(app)
-  //     .post("/api/user/login").send({ 
-  //       userName: 'jack1',
-  //       password: 'testtest' 
-  //     });
-  //     expect(result.status).toBe(200);
+      expect(result.status).toBe(200);
 
-  //     const json = JSON.parse(result.text);
-  //     expect(json.success).toBe(false);
-  //     expect(json.data).toBe(null);
-  //   });
+      const json = JSON.parse(result.text);
+      expect(json.success).toBe(false);
+      expect(json.data).toBeNull();
+    });
 
-  //   it("should POST /api/user/login 密码长度少于6位", async () => {
-  //     // case3_1: 密码长度少于6位
-  //     const result = await createHttpRequest(app)
-  //     .post("/api/user/login").send({ 
-  //       userName: 'jack',
-  //       password: 'test' 
-  //     })
-  //     expect(result.status).toBe(422);
-  //   });
+    it("用户名输入不匹配,即是未注册的用户", async () => {
+      // case2_2: 用户名不匹配
+      const result = await createHttpRequest(app)
+      .post("/api/user/login").send({ 
+        userName: 'jack1',
+        password: 'testtest' 
+      });
+      expect(result.status).toBe(200);
 
-  //   it("should POST /api/user/login 密码长度大于8位", async () => {
-  //     // case3_2: 密码长度大于8位
-  //     const result = await await createHttpRequest(app)
-  //     .post("/api/user/login").send({ 
-  //       userName: 'jack',
-  //       password: 'testtesttest' 
-  //     })
-  //     expect(result.status).toBe(422);
-  // });
+      const json = JSON.parse(result.text);
+      expect(json.success).toBe(false);
+      expect(json.data).toBe(null);
+    });
 
-  // it("should POST /api/user/login 用户名长度为0", async () => {
-  //     // case3_3: 用户名长度为0
-  //     const result = await createHttpRequest(app)
-  //     .post("/api/user/login").send({ 
-  //       userName: '',
-  //       password: 'testtest' 
-  //     })
-  //     expect(result.status).toBe(422);
-  //   });
-  //   it("should POST /api/user/login 密码长度少于6位", async () => {
-  //     // case3_4: 用户名长度为12
-  //     const result = await createHttpRequest(app)
-  //     .post("/api/user/login")
-  //     .send({ 
-  //       userName: 'jackjackjackjack',
-  //       password: 'testtest' 
-  //     })
-  //     expect(result.status).toBe(422);
-  //   });
-  // });
+    it("输入密码长度少于6位", async () => {
+      // case3_1: 密码长度少于6位
+      const result = await createHttpRequest(app)
+      .post("/api/user/login").send({ 
+        userName: 'jack',
+        password: 'test' 
+      })
+      expect(result.status).toBe(422);
+    });
+
+    it("输入密码长度大于8位", async () => {
+      // case3_2: 密码长度大于8位
+      const result = await await createHttpRequest(app)
+      .post("/api/user/login").send({ 
+        userName: 'jack',
+        password: 'testtesttest' 
+      })
+      expect(result.status).toBe(422);
+    });
+
+    it("输入用户名长度小于最小长度", async () => {
+      // case3_3: 用户名长度为0
+      const result = await createHttpRequest(app)
+      .post("/api/user/login").send({ 
+        userName: '',
+        password: 'testtest' 
+      })
+      expect(result.status).toBe(422);
+    });
+    it("输入用户名长度大于最大长度", async () => {
+      // case3_4: 用户名长度为80
+      const result = await createHttpRequest(app)
+      .post("/api/user/login")
+      .send({ 
+        userName: 'jackjackjackjackjackjackjackjackjackjackjackjackjackjackjackjackjackjackjackjack',
+        password: 'testtest' 
+      })
+      expect(result.status).toBe(422);
+    });
+  });
   describe("POST /api/user/register", () => {
-    let app: Application;
+    // let app: Application;
 
-    beforeAll(async () => {
-      // create app
-      app = await createApp<Framework>();
-    });
+    // beforeAll(async () => {
+    //   // create app
+    //   app = await createApp<Framework>();
+    // });
 
-    afterAll(async () => {
-      await close(app);
-    });
+    // afterAll(async () => {
+    //   await close(app);
+    // });
     it("插入新用户，返回成功", async () => {
       const timestamp = (new Date()).getTime();
       // make request
